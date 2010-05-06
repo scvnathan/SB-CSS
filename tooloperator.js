81f1cc52b7cb787f896440bdee61adbcc89fcd7c
bonecandy <Bonecandy@Computron.local>
2010-05-05 20:41:34 -0400

    renamed css selectors

--- tooloperator.js
+++ tooloperator.js
@@ -1,58 +1,59 @@
-	(function($) {
-		$.fn.extend({
-			toolOperator :function(options) {
-				var selector = $(this).selector;
-				var defaults = {
-					textElem : null,
-					text : "tooltip!",					
-					outerDiv: true,
-					delegateElem : null,
-					callback : function(){}						
+(function($) {
+	$.fn.extend({
+		toolOperator :function(options) {
+			var selector = $(this).selector;
+			var defaults = {
+				textElem : null,
+				text : "tooltip!",	
+				attr : 'title',				
+				outerDiv: true,
+				delegateElem : null,
+				callback : function(){}						
+			}
+			var options = $.extend(defaults, options);
+			function showTip(text,obj) {
+				var sbTip = obj.find('.toolOp');
+				if (sbTip.length<=0) {
+					var tipStr =(options.outerDiv) ? "<div class='toolOpOuter'><span class='toolOp'>&#10230;"+text+"</span></div>" : "<div class='toolOp'>&#10230;"+text+"</div>"
+					obj.append(tipStr);
+				} else {
+					sbTip.show();
 				}
-				var options = $.extend(defaults, options);
-				function showTip(text,obj) {
-					var sbTip = obj.find('.sbTipInner');
-					if (sbTip.length<=0) {
-						var tipStr =(options.outerDiv) ? "<div class='sbTipOuter'><span class='sbTipInner'>&#10230;"+text+"</span></div>" : "<div class='sbTipInner'>&#10230;"+text+"</div>"
-						obj.append(tipStr);
-					} else {
-						sbTip.show();
-					}
-					(options.callback)(obj);
-					
-				}				
-				function hideTip(obj) {
-					obj.find('.sbTipInner, .sbTipOuter').hide();
-				}
-				this.destroy = function(t) {
-					t.find('.sbToolTip').remove();		
-					if(options.delegateElem!=null) {
-						options.delegateElem.undelegate();
-					}			
-				}
-
+				(options.callback)(obj);
 				
-				return this.each(function(){
-					var o = options;
-					var text = $(this).find(o.textElem).attr('title');
-					if (o.delegateElem == null) {					
-						$(this).bind("mouseenter",function(){
-							showTip(text,$(this));
-						});
-						$(this).bind("mouseleave",function(){
-							hideTip($(this));
-						});
-						
-					} else {
-						o.delegateElem.delegate(selector,'mouseenter',function(){
-							showTip($(this).find(o.textElem).attr('title'),$(this));
-						});
-						o.delegateElem.delegate(selector,'mouseleave',function(){
-							hideTip($(this));
-						});					
-					}
-					return this
-				})		
-			}			
-		});
-	})(jQuery);
+			}				
+			function hideTip(obj) {
+				obj.find('.toolOp, .sbTipOuter').hide();
+			}
+			this.destroy = function(t) {
+				t.find('.toolOpOuter,.toolOp').remove();		
+				if(options.delegateElem!=null) {
+					options.delegateElem.undelegate();
+				}			
+			}
+
+			
+			return this.each(function(){
+				var o = options;
+				var text = $(this).find(o.textElem).attr(o.attr);
+				if (o.delegateElem == null) {					
+					$(this).bind("mouseenter",function(){
+						showTip(text,$(this));
+					});
+					$(this).bind("mouseleave",function(){
+						hideTip($(this));
+					});
+					
+				} else {
+					o.delegateElem.delegate(selector,'mouseenter',function(){
+						showTip($(this).find(o.textElem).attr(o.attr),$(this));
+					});
+					o.delegateElem.delegate(selector,'mouseleave',function(){
+						hideTip($(this));
+					});					
+				}
+				return this
+			})		
+		}			
+	});
+})(jQuery);
